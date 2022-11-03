2ca7c8393e958f47bc34bf1946e81547e3a79958
Gordex2014 <alvaromsd_11@hotmail.com>
2022-11-03 10:47:28 -0400

    Dummy users added

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { VideoPlayer } from "../../components";
 import { useRecording } from "../../hooks";
+import { userNames } from "../../constants";
 
 import styles from "./MainPage.module.css";
 
 export const MainPage = () => {
+  // Random userName
+  const [userName] = useState(
+    userNames[Math.floor(Math.random() * userNames.length)]
+  );
+
   const {
     askPermission: askCameraPermission,
     isRecording: isCameraRecording,
@@ -19,23 +25,24 @@ export const MainPage = () => {
     stopRecording: stopScreenRecording,
   } = useRecording("screen", "JohnDoeScreen");
 
-  const handleClick = async () => {
+  const handleAskPermissions = async () => {
     askCameraPermission();
     askScreenPermission();
   };
 
-  const handleStart = async () => {
+  const handleStartRecording = async () => {
     startCameraRecording();
     startScreenRecording();
   };
 
-  const handleStop = async () => {
+  const handleStopRecording = async () => {
     stopCameraRecording();
     stopScreenRecording();
   };
 
   return (
     <React.Fragment>
+      <h1 className={styles.title}>{userName}</h1>
       <div className={styles.videoContainer}>
         <div className={styles.videoBlock}>
           <VideoPlayer></VideoPlayer>
@@ -48,9 +55,9 @@ export const MainPage = () => {
         </div>
       </div>
       <div className={styles.buttonsContainer}>
-        <button onClick={handleClick}>Ask Permissions</button>
-        <button onClick={handleStart}>Start recording</button>
-        <button onClick={handleStop}>Stop recording</button>
+        <button onClick={handleAskPermissions}>Ask Permissions</button>
+        <button onClick={handleStartRecording}>Start recording</button>
+        <button onClick={handleStopRecording}>Stop recording</button>
       </div>
     </React.Fragment>
   );
